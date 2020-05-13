8abd4c0284ad179205a25103d4654dece26db1d5
Vrndavana <Vrmdavama.th@gmail.com>
2020-05-12 20:11:30 -0700

    Returning muh list

--- api/server.js
+++ api/server.js
@@ -28,15 +28,21 @@ server.get("/:id", (req,res) =>{
 // server.get = push 200 down to add an if else, if checks if empty FIRST!!!!
 
 // for req.body to work you need all required feilds filled
+// server.post("/", (req, res) => {
+//     db('cars').insert(req.body)
+//     .then(count => { db('cars').where({id:count})
+//     .then(vehicles=>{
+//        if(vehicles.length === 0) { res.status(404).json({message: "LIIESS"})} 
+//        else {res.status(200).json({data:vehicles} )}})
+//     .catch(err=>{res.status(500).json({message:err})})})
+// })
+// above would add a new car to count but not return the list 
 server.post("/", (req, res) => {
     db('cars').insert(req.body)
-    .then(count => { db('cars').where({id:count})
-    .then(vehicles=>{
-       if(vehicles.length === 0) { res.status(404).json({message: "LIIESS"})} 
-       else {res.status(200).json({data:vehicles} )}})
+    .then(count => { db('cars')
+    .then(vehicles=>{res.status(200).json({data:vehicles})})
     .catch(err=>{res.status(500).json({message:err})})})
 })
-
 
 
 
